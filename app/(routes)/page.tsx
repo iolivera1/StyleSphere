5640212e1e1fee4425a6d06b230b04b6ccaed81e
Ivan Olivera <ivansinhox@gmail.com>
2023-08-10 19:41:26 -0300

    finish

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,26 @@
+import getBillboard from "@/actions/get-billboard";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/products-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("a57c6d58-fef2-429a-b9ce-9960f07eca15");
+  console.log(billboard)
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} /> {/* fix this */}
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
