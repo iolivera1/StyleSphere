bb80774c61906a47447f6100bc3f14d3afc0fd83
Ivan Olivera <ivansinhox@gmail.com>
2023-08-13 23:32:29 -0300

    fix

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,16 +1,20 @@
-"use client;";
+"use client";
 
-import { cn } from "@/lib/utils";
-import { Category } from "@/types";
-import Link from "next/link";
+import Link from "next/link"
 import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils"
+import { Category } from "@/types";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({ data }) => {
-  const pathname = usePathname;
+const MainNav: React.FC<MainNavProps> = ({
+  data
+}) => {
+  const pathname = usePathname();
+
   const routes = data.map((route) => ({
     href: `/category/${route.id}`,
     label: route.name,
@@ -18,21 +22,23 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   }));
 
   return (
-    <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
+    <nav
+      className="mx-6 flex items-center space-x-4 lg:space-x-6"
+    >
       {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
           className={cn(
-            "text-sm font-medium transition-colors hover:text-black",
-            route.active ? "text-black" : "text-neutral-500"
+            'text-sm font-medium transition-colors hover:text-black',
+            route.active ? 'text-black' : 'text-neutral-500'
           )}
         >
           {route.label}
-        </Link>
+      </Link>
       ))}
     </nav>
-  );
+  )
 };
 
 export default MainNav;
