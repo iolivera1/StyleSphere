07cdcdea7472eccf83794e2c1b9af3dd76083916
ivan <ivansinhox@gmail.com>
2024-12-31 01:16:09 -0300

    changes for deploy

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -11,19 +11,18 @@ import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
-type Params = {
-  categoryId: string;
-};
+type Params = Promise<{ categoryId: string }>;
 
 type CategoryPageProps = {
-  params: Promise<Params>;
+  children: React.ReactNode;
+  params: Params;
   searchParams: {
     colorId: string;
     sizeId: string;
   };
 };
 
-export default async function CategoryPage(props: { params: Promise<Params>; searchParams: { colorId: string; sizeId: string } }) {
+export default async function CategoryPage(props: CategoryPageProps) {
   const params = await props.params;
   const { categoryId } = params;
 
@@ -38,27 +37,28 @@ export default async function CategoryPage(props: { params: Promise<Params>; sea
   const category = await getCategory(categoryId);
 
   return (
-    <div className="bg-white">
-      <Container>
-        <Billboard data={category.billboard} />
-        <div className="px-4 sm:px-6 lg:px-8 pb-24">
-          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            <MobileFilters sizes={sizes} colors={colors} />
-            <div className="hidden lg:block">
-              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
-              <Filter valueKey="colorId" name="Colors" data={colors} />
-            </div>
-            <div className="mt-6 lg:col-span-4 lg:mt-0">
-              {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                {products.map((item) => (
-                  <ProductCard key={item.id} data={item} />
-                ))}
+      <div className="bg-white">
+        <Container>
+          <Billboard data={category.billboard} />
+          <div className="px-4 sm:px-6 lg:px-8 pb-24">
+            <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+              <MobileFilters sizes={sizes} colors={colors} />
+              <div className="hidden lg:block">
+                <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+                <Filter valueKey="colorId" name="Colors" data={colors} />
+              </div>
+              <div className="mt-6 lg:col-span-4 lg:mt-0">
+                {products.length === 0 && <NoResults />}
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                  {products.map((item) => (
+                      <ProductCard key={item.id} data={item} />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Container>
-    </div>
+        </Container>
+        {props.children}
+      </div>
   );
 }
